c6e220d31b942559dc0940752ed423fa96048f52
Aysel <aysel@gmail.com>
2026-04-30 14:31:00 +0400

    usePomodoro hook added

--- src/App.tsx
+++ src/App.tsx
@@ -1,62 +1,27 @@
-import { useEffect, useState } from "react";
+import { usePomodoro } from "./hooks/usePomodoro";
 import Clock from "./Clock";
 import Timer from "./Timer";
 
 function App() {
-  const [isRunning, setIsRunning] = useState<boolean>(false);
-  const [pause, setPause] = useState<boolean>(false);
-  const [step, setStep] = useState<"work" | "break">("work");
-  const [timeLeft, setTimeLeft] = useState(30);
-
-  const toggleStep = () => {
-    setStep((prev) => {
-      const next = prev === "work" ? "break" : "work";
-      setTimeLeft(next === "work" ? 30 : 15);
-      return next;
-    });
-  };
-
-  useEffect(() => {
-    if (!isRunning || pause) return;
-
-    const timer = setInterval(() => {
-      setTimeLeft((prev) => {
-        if (prev <= 1) {
-          const nextStep = step === "work" ? "break" : "work";
-          setStep(nextStep);
-          return nextStep === "work" ? 30 : 15;
-        }
-
-        return prev - 1;
-      });
-    }, 1000);
-
-    return () => clearInterval(timer);
-  }, [isRunning, pause, step]);
+  const { mode, timeLeft, isRunning, round, start, pause, reset, skip } =
+    usePomodoro({ focusTime: 10, shortBreakTime: 5, longBreakTime: 7 });
 
   return (
     <div className="container">
       <div className="timer">
         <div className="buttons">
-          <button
-            className="controls"
-            onClick={() => {
-              setIsRunning((prev) => !prev);
-              setStep("work");
-              setTimeLeft(30);
-            }}
-          >
-            {isRunning ? "Reset" : "Start"}
+          <button className="controls" onClick={start}>
+            Start
           </button>
-          <button
-            className={`pause ${pause ? "active" : ""}`}
-            onClick={() => setPause((prev) => !prev)}
-          >
-            {pause ? "Continue" : "Pause"}
+          <button className="pause" onClick={pause}>
+            {isRunning ? "Pause" : "Resume"}
           </button>
-          <button className={`step step-${step}`} onClick={toggleStep}>
-            Next
-          </button>
+          <button onClick={reset}>Reset</button>
+          <button onClick={skip}>Skip</button>
+        </div>
+        <div className="info">
+          <p>Mode: {mode}</p>
+          <p>Round: {round}</p>
         </div>
         <div className="clock">
           <Clock />
---
+++ src/hooks/usePomodoro.ts
@@ -0,0 +1,88 @@
+import { useState, useEffect } from "react";
+
+type TMode = "focus" | "shortBreak" | "longBreak";
+
+interface IConfig {
+  focusTime?: number;
+  shortBreakTime?: number;
+  longBreakTime?: number;
+  roundLimit?: number;
+}
+
+export function usePomodoro(config?: IConfig) {
+  const focus = config?.focusTime ?? 30;
+  const short = config?.shortBreakTime ?? 15;
+  const long = config?.longBreakTime ?? 60;
+  const roundLimit = config?.roundLimit ?? 4;
+
+  const [mode, setMode] = useState<TMode>("focus");
+  const [timeLeft, setTimeleft] = useState<number>(focus);
+  const [isRunning, setIsRunning] = useState<boolean>(false);
+  const [round, setRound] = useState<number>(1);
+
+  const start = () => setIsRunning(true);
+  const pause = () => setIsRunning((prev) => !prev);
+
+  const reset = () => {
+    setIsRunning(false);
+    setRound(1);
+    setTimeleft(focus);
+    setMode("focus");
+  };
+
+  const getNextMode = (currentMode: TMode, currentRound: number): TMode => {
+    if (currentMode === "focus") {
+      return currentRound >= roundLimit ? "longBreak" : "shortBreak";
+    }
+    return "focus";
+  };
+
+  const getTimeInterval = (mode: TMode): number => {
+    return mode === "focus" ? focus : mode === "shortBreak" ? short : long;
+  };
+
+  const switchMode = () => {
+    const prevRound = round;
+    if (mode === "focus") {
+      const nextRound = prevRound >= roundLimit ? 1 : prevRound + 1;
+      setRound(nextRound);
+    }
+    const nextMode = getNextMode(mode, round);
+    setMode(nextMode);
+    setTimeleft(getTimeInterval(nextMode));
+  };
+
+  const skip = () => {
+    switchMode();
+  };
+
+  useEffect(() => {
+    if (!isRunning) return;
+
+    const interval = setInterval(() => {
+      setTimeleft((prev) => {
+        if (prev <= 1) {
+          setTimeout(() => {
+            switchMode();
+          }, 300);
+
+          return 0;
+        }
+
+        return prev - 1;
+      });
+    }, 1000);
+    return () => clearInterval(interval);
+  }, [isRunning, mode]);
+
+  return {
+    mode,
+    timeLeft,
+    isRunning,
+    round,
+    start,
+    pause,
+    reset,
+    skip,
+  };
+}
